ae69bc1f548dd9c80ebd10122fddb188fe287a82
jhelm37174 <jamiehelm@hotmail.com>
2014-10-31 16:04:54 +0700

    Added additional test cases

--- tests/beautifytimeTest.php
+++ tests/beautifytimeTest.php
@@ -5,5 +5,25 @@ class BeautifyTest extends PHPUnit_Framework_TestCase
   {
     $this->assertTrue(beautifytime(12) == "0h 0m 12s");
   }
+
+  public function test_beautifytime2()
+  {
+    $this->assertTrue(beautifytime(3600) == "1h 0m 0s");
+  }
+
+  public function test_beautifytime3()
+  {
+    $this->assertTrue(beautifytime(3615) == "1h 0m 15s");
+  }
+
+  public function test_beautifytime4()
+  {
+    $this->assertTrue(beautifytime(136687) == "37h 58m 7s");
+  }
+
+  public function test_beautifytime5()
+  {
+    $this->assertTrue(beautifytime(41548533) == "11547h 15m 33s");
+  } 
 }
 
